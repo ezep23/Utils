5f5da1e13ed11c3af67b5c92bd6fdc49b89e55c8
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-09-11 23:26:11 -0300

    btn dark-white

---
+++ main.js
@@ -0,0 +1,10 @@
+// Opción 1 (Por ahora)
+const bg = document.querySelector("body");
+
+function bgWhite(){
+    bg.style.backgroundColor = "#ffffff";
+};
+
+function bgBlack(){
+    bg.style.backgroundColor = "#000000";
+};
